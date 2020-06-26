82cd12ffd3784b4ea6554707c6f7a9b3fd329630
Nicola Zorzo <nicola.zorzo@gmail.com>
2020-06-26 21:35:02 +0200

    auto-deployment via script

--- deployment.bat
+++ deployment.bat
@@ -1,4 +1,10 @@
 @echo off
+
+echo =========================
+echo AUTO-DEPLOYMENT TO GITHUB
+echo =========================
+
+:: echo ** Building project
 :: ng build --prod --output-path docs --base-href /wotlk-arena-points/
 
 echo ** Moving to ./docs
@@ -7,4 +13,17 @@ cd docs
 echo ** Copying index.html into 404.html
 copy .\index.html .\404.html
 
+echo ** Moving back to main folder
+cd ..
+
+echo ** Stage changes
+git add .
+
+echo ** Commit changes
+git commit -m "auto-deployment via script"
+
+echo ** Push changes
+git push origin master
+
+echo ** Deployment finished!
 pause
